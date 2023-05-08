658dcf586c230b360a3270d251991175f9347195
EstimalTi <76611287+EstimalTi@users.noreply.github.com>
2023-05-08 21:12:17 +0900

    Update and rename number_divisors.py to number's_divisors.py

--- number's_divisors.py
+++ number's_divisors.py
@@ -1,9 +1,10 @@
 n = int(input())
 # находит все делители чисел от 0 до n
-for i in range(1, n+1):
-    print(i, '- ', end='')
-    for k in range(1, n+1):
+for i in range(1, n + 1):
+    print(i, end='')
+    print(': ', end='')
+    for k in range(1, n + 1):
         if i % k == 0:
             print(k, end='')
-            print(', ', end='')
+            print('  ', end='')
     print()
